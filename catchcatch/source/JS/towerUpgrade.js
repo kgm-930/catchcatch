f598bfd64fc570a95ade439ce175fb9c53212168
CYoungSun <vnddns26@naver.com>
2022-10-19 22:17:59 +0900

    #1 :see_no_evil: 프로젝트 구조 수정 및 config파일 수정

---
+++ catchcatch/source/JS/towerUpgrade.js
@@ -0,0 +1,26 @@
+export default function tower() {
+  const upgradeContent = document.querySelector(".upgradeContent");
+  upgradeContent.innerHTML = `<div class="towerIcons">
+            <div>그림</div>
+            <div>그림</div>
+        </div>
+        <div class="towerIcons">
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+        </div>
+        <div class="towerIcons">
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+        </div>
+        <div class="towerIcons">
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+            <div>그림</div>
+        </div>`;
+}
