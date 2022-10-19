112bd40a474ce913c9252a01a6a8d150271c79b7
CYoungSun <vnddns26@naver.com>
2022-10-19 22:38:03 +0900

    #1 :see_no_evil: 폴더구조 수정 완료

--- app.js
+++
@@ -1,7 +0,0 @@
-import StartPageInit from "./JS/StartPage.js";
-
-function Init() {
-  StartPageInit(); //스타트 페이지 init
-}
-
-Init();
--- catchcatch/webpack.config.js
+++ catchcatch/webpack.config.js
@@ -12,6 +12,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(gif|svg|jpg|png)$/,
+        loader: "file-loader",
+      },
+      {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
