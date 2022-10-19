33a20424a3c87260161c1f50f9fc4f1181fb732b
CYoungSun <vnddns26@naver.com>
2022-10-19 22:38:03 +0900

    #1 :see_no_evil: 폴더구조 수정 완료

--- catchcatch/source/main.js
+++ catchcatch/source/main.js
@@ -1,4 +1,10 @@
 import "./CSS/upgrade.css";
 import "./CSS/index.css";
-import initUpgrade from "./JS/upgrade";
-initUpgrade();
+// import initUpgrade from "./JS/upgrade";
+import StartPageInit from "./JS/StartPage.js";
+
+function Init() {
+  StartPageInit(); //스타트 페이지 init
+}
+
+Init();
