e83a52e39eea92192c62224dc6deaaa6e990ff89
최영선 <vnddns26@naver.com>
2022-10-21 11:28:56 +0900

    #1 :sparkles: 특정 조건일때 특성창 띄우기

--- catchcatch/source/JS/levelup.js
+++ catchcatch/source/JS/levelup.js
@@ -1,26 +1,44 @@
 export default function levelup() {
   const app = document.querySelector("#app");
   app.innerHTML = `
-    <div class="levelupContainer">
+  <div class="levelupContainer">
       <div class="levelupContent">
-        <div class="levelupImg">특성 아이콘</div>
-        <div class="levelupText">설명</div>
+      <div class="levelupImg">특성 아이콘</div>
+      <div class="levelupText">설명</div>
       </div>
       <div class="levelupContent">
-        <div class="levelupImg">특성 아이콘</div>
-        <div class="levelupText">설명</div>
+      <div class="levelupImg">특성 아이콘</div>
+      <div class="levelupText">설명</div>
       </div>
       <div class="levelupContent">
-        <div class="levelupImg">특성 아이콘</div>
-        <div class="levelupText">설명</div>
+      <div class="levelupImg">특성 아이콘</div>
+      <div class="levelupText">설명</div>
       </div>
-    </div>
+      </div>
   `;
   const contents = document.querySelectorAll(".levelupContent");
+  const levelupContainer = document.querySelector(".levelupContainer");
   console.log(contents);
   for (let i = 0; i < 3; i++) {
     contents[i].addEventListener("click", () => {
       console.log("특성");
+      levelupContainer.style.display = "none";
+      expUp();
     });
   }
 }
+
+function expUp() {
+  const interval = setInterval(() => {
+    console.log(exp);
+    exp++;
+    const levelupContainer = document.querySelector(".levelupContainer");
+    if (exp === 3) {
+      clearInterval(interval);
+      levelupContainer.style.display = "flex";
+      exp = 0;
+    }
+  }, 1000);
+}
+
+expUp();
--- catchcatch/source/main.js
+++ catchcatch/source/main.js
@@ -5,6 +5,8 @@ import initUpgrade from "./JS/upgrade";
 import StartPageInit from "./JS/StartPage.js";
 import levelup from "./JS/levelup.js";
 
+global.exp = 0;
+
 function Init() {
   StartPageInit(); //스타트 페이지 init
 }
