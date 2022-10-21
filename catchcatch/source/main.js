9f634b2ca37742d238907b908db61a013ea43c45
최영선 <vnddns26@naver.com>
2022-10-21 16:20:56 +0900

    #1 :poop: 레벨업 이벤트 구조 변경 필요

--- catchcatch/source/main.js
+++ catchcatch/source/main.js
@@ -4,11 +4,13 @@ import "./CSS/levelup.css";
 import initUpgrade from "./JS/upgrade";
 import StartPageInit from "./JS/StartPage.js";
 import levelup from "./JS/levelup.js";
+import inGameUI from "./JS/inGameUI.js";
 
 global.exp = 0;
-
 function Init() {
   StartPageInit(); //스타트 페이지 init
+  // inGameUI();
 }
 levelup();
+inGameUI();
 // Init();
