ad23fb0b628f96ac503c22c7d8c463e012474925
추희원 <colorfulvox@gmail.com>
2022-10-25 17:38:33 +0900

    #1 :sparkles: 캐릭터 구성 구현

--- catchcatch/source/JS/UI/CharSpace.js
+++ catchcatch/source/JS/UI/CharSpace.js
@@ -3,136 +3,148 @@ import { StartBtnOn } from "./StartPage";
 import { SaveData } from "../../main.js";
 import { config } from "../GAME/game.js";
 
-var _CharSpace;
+var _SettingSpace;
 
 var _SlideList;
+var _BuyBtn;
+var _GameStartBtn;
+var _CanCount;
 
 const CharPageInit = () => {
   //여기서 미리 서버 정보를 가져온다. ---------------------------
   var _AllBtn = document.getElementById("AllBtnList");
   // StartPage가 생성되면 StartPage에 CharPage를 넣는것으로 하자
-  _CharSpace = document.createElement("div");
-  _CharSpace.className = "CharSpace";
-  _CharSpace.style.display = "none";
+  _SettingSpace = document.createElement("div");
+  _SettingSpace.className = "SettingSpace";
+  _SettingSpace.style.display = "none";
 
-  _AllBtn.appendChild(_CharSpace);
+  _AllBtn.appendChild(_SettingSpace);
   //--------------------------------------------------
 
-  // // ChoiceChar = 캐릭터 템플릿 + Slide 버튼
-  // var _ChoiceChar = document.createElement("div");
-  // _ChoiceChar.className = "ChoiceChar";
-  // _CharSpace.appendChild(_ChoiceChar);
+  //뒤로가기
+  var _BackIcon = document.createElement("button");
+  _BackIcon.className = "BackIcon";
+  _SettingSpace.appendChild(_BackIcon);
+  _BackIcon.addEventListener("click", BackStart);
 
-  // const _Slide = document.createElement("div");
-  // _Slide.className = "Slide";
-  // _ChoiceChar.appendChild(_Slide);
+  // 캔 갯수 표시
+  var _CanIcon = document.createElement("div");
+  _CanIcon.className = "CanIcon";
+  _SettingSpace.appendChild(_CanIcon);
 
-  // //캐릭터 슬라이드 리스트
-  // _SlideList = document.createElement("ul");
-  // _SlideList.className = "SlideList";
-  // _Slide.appendChild(_SlideList);
+  _CanCount = document.createElement("div");
+  _CanCount.className = "CanCount";
+  _CanCount.textContent = "x" + LocalData.Coin;
+  _CanIcon.appendChild(_CanCount);
 
-  // //캐릭터 템플릿 5개 생성
-  // for (let i = 0; i < 5; ++i) {
-  //   const _CharTemplate = document.createElement("li");
+  //고양이 구매
+  _BuyBtn = document.createElement("button");
+  _BuyBtn.className = "BuyBtn";
+  _SettingSpace.appendChild(_BuyBtn);
+  _BuyBtn.textContent = "100 Can!";
+  _BuyBtn.style.display = "none";
+  _BuyBtn.addEventListener("click", BuyChar);
 
-  //   _CharTemplate.className = "CharTemplate";
+  // 캐릭터 선택 Space
+  var _CharSpace = document.createElement("div");
+  _CharSpace.className = "CharSpace";
+  _SettingSpace.appendChild(_CharSpace);
 
-  //   // CharObj = 이미지 + 텍스트
-  //   const _CharObj = document.createElement("div");
-  //   _CharObj.className = "CharObj";
+  //컨텐츠 네임 [캐릭터선택]
+  var _ContentName = document.createElement("div");
+  _ContentName.className = "ContentName";
+  _ContentName.textContent = "[캐릭터선택]";
+  _CharSpace.appendChild(_ContentName);
 
-  //   // 캐릭터 이미지
+  // 캐릭턴 선택 박스
+  var _ChoiceCharBox = document.createElement("div");
+  _ChoiceCharBox.className = "ChoiceCharBox";
+  _CharSpace.appendChild(_ChoiceCharBox);
 
-  //   var CharImg = document.createElement("div");
-  //   CharImg.className = "CharImg";
-  //   // CharImg.style.background = url(`images/CharImg/${i}.png`);
-  //   // CharImg.width = 150;
-  //   // CharImg.height = 150;
-  //   // CharImg.style.margin = "10px";
-  //   // CharImg.style.position = "absolute";
+  // LEFT Btn
+  var _LeftBtn = document.createElement("button");
+  _LeftBtn.className = "CharLeftBtn";
+  _LeftBtn.addEventListener("click", SlideLeft);
+  _ChoiceCharBox.appendChild(_LeftBtn);
 
-  //   _CharObj.appendChild(CharImg);
+  //슬라이드
+  var _Slide = document.createElement("div");
+  _Slide.className = "Slide";
+  _ChoiceCharBox.appendChild(_Slide);
 
-  //   var CharContxt = document.createElement("div");
-  //   CharContxt.className = "CharContxt";
-  //   // CharContxt.textContent =
-  //   //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer eu maximus purus. In hac habitasse platea dictumst. Aenean sit amet mauris elit. Aliquam posuere pharetra ipsum, eu ullamcorper arcu tincidunt id. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer sit amet ipsum est. Nulla sodales felis sit amet velit commodo, fringilla fringilla nisi posuere. Aenean tempor cursus augue a blandit. Curabitur tempus ante ex. Praesent ullamcorper finibus erat quis laoreet. Vestibulum volutpat metus quis blandit placerat.";
-  //   // CharContxt.style.margin = "10px";
-  //   _CharObj.appendChild(CharContxt);
+  _SlideList = document.createElement("div");
+  _SlideList.className = "SlideList";
+  _Slide.appendChild(_SlideList);
 
-  //   // 구매 버튼
-  //   if (LocalData.Cat[i] === false) {
-  //     // var Buybtn = document.createElement("button");
-  //     // Buybtn.textContent = "100";
-  //     // Buybtn.style.position = "absolute";
-  //     // Buybtn.style.left = "20%";
-  //     // Buybtn.style.bottom = "0%";
-  //     // Buybtn.style.width = "100px";
-  //     // Buybtn.id = `${i}`;
-  //     // Buybtn.addEventListener("click", BuyChar);
-  //     // _CharObj.appendChild(Buybtn);
-  //   } else {
-  //     if (ChoiceCat === -1) ChoiceCat = i;
-  //   }
-  //   // _CharTemplate <= CharOBj
-  //   _CharTemplate.appendChild(_CharObj);
+  const catnamelist = [
+    "캐　츠",
+    "용냥이",
+    "무냥이",
+    "영냥이",
+    "진냥이",
+    "규냥이",
+    "희냥이",
+  ];
 
-  //   _SlideList.appendChild(_CharTemplate);
-  // }
+  for (let i = 0; i < 7; ++i) {
+    var _CharTemp = document.createElement("div");
+    _CharTemp.className = "CharTemp";
 
-  // //Left - Slide버튼
-  // const _LeftBtn = document.createElement("button");
-  // _LeftBtn.className = "ChoiceBtn";
-  // _ChoiceChar.appendChild(_LeftBtn);
+    //캐릭터 이미지 가져오기
+    var _CharImg = document.createElement("img");
+    _CharImg.className = "CharImg";
+    _CharImg.src = `images/CharImg/${i}.png`;
+    // _CharImg.style.border = "3px solid black";
+    _CharTemp.appendChild(_CharImg);
 
-  // _LeftBtn.addEventListener("click", SlideLeft);
+    //캐릭 이름 가져오기
+    var _CharName = document.createElement("div");
+    _CharName.className = "CharName";
+    _CharName.textContent = "[" + catnamelist[i] + "]";
+    _CharTemp.appendChild(_CharName);
 
-  // //Right - Slide버튼
-  // const _RightBtn = document.createElement("button");
-  // _RightBtn.className = "ChoiceBtn";
-  // _ChoiceChar.appendChild(_RightBtn);
+    _SlideList.appendChild(_CharTemp);
+  }
 
-  // _RightBtn.addEventListener("click", SlideRight);
-  // //--------------------------------------------------
+  //RightBtn
+  var _RightBtn = document.createElement("button");
+  _RightBtn.className = "CharRightBtn";
+  _RightBtn.addEventListener("click", SlideRight);
+  _ChoiceCharBox.appendChild(_RightBtn);
 
-  // // CharBtnList = 뒤로가기 + 게임시작 + 난이도
-  // const _CharBtnList = document.createElement("div");
-  // _CharBtnList.className = "CharBtnList";
-  // _CharSpace.appendChild(_CharBtnList);
+  //--------------------------------------------
 
-  // // 뒤로가기
-  // const _BackStartBtn = document.createElement("button");
-  // _BackStartBtn.className = "CharBtn";
-  // _BackStartBtn.textContent = "뒤로가기";
-  // // 이벤트 리스너 추가 ------------------------
-  // _BackStartBtn.addEventListener("click", BackStart);
-  // // -------------------------------------
-  // _CharBtnList.appendChild(_BackStartBtn);
+  //난이도 선택 및 게임 시작
+  var _OtherBtn = document.createElement("div");
+  _OtherBtn.className = "OtherBtn";
+  _SettingSpace.appendChild(_OtherBtn);
 
-  // // 게임시작
-  // const _StartGameBtn = document.createElement("button");
-  // _StartGameBtn.className = "CharBtn";
-  // _StartGameBtn.textContent = "게임 시작";
-  // _CharBtnList.appendChild(_StartGameBtn);
-  // _StartGameBtn.addEventListener("click", GameStart);
+  var _LevelBtn = document.createElement("button");
+  _LevelBtn.className = "LevelBtn";
+  _LevelBtn.textContent = "NORMAL";
+  _LevelBtn.style.background =
+    "url('images/ui/MapLevelNormal.png') no-repeat center";
+  _LevelBtn.style.backgroundSize = "contain";
+  _LevelBtn.addEventListener("click", MapLevel);
+  _OtherBtn.appendChild(_LevelBtn);
 
-  // // 난이도 설정 임시로 버튼
-  // const _MapLevel = document.createElement("button");
-  // _MapLevel.className = "CharBtn";
-  // _MapLevel.textContent = "일반 모드";
-  // _CharBtnList.appendChild(_MapLevel);
-  // _MapLevel.addEventListener("click", MapLevel);
+  _GameStartBtn = document.createElement("button");
+  _GameStartBtn.className = "GameStartBtn";
+  _GameStartBtn.id = "GameStartBtn";
+  _GameStartBtn.textContent = "GO!CATCH!";
+  _GameStartBtn.style.textAlign = "center";
+  _GameStartBtn.addEventListener("click", GameStart);
+  _OtherBtn.appendChild(_GameStartBtn);
 };
 
 export default CharPageInit;
 
 export const CharSpaceOn = () => {
-  _CharSpace.style.display = "flex";
+  _SettingSpace.style.display = "flex";
 };
 
 export const CharSpaceOff = () => {
-  _CharSpace.style.display = "none";
+  _SettingSpace.style.display = "none";
 };
 
 // 돌아가기
@@ -158,27 +170,49 @@ let CharIndex = 0;
 function SlideLeft() {
   if (CharIndex > 0) {
     --CharIndex;
-    _SlideList.style.left = -CharIndex * 760 + "px";
-    if (LocalData.Cat[CharIndex] != false) ChoiceCat = CharIndex;
-    else ChoiceCat = -1;
+    _SlideList.style.left = -CharIndex * 190 + "px";
+    if (LocalData.Cat[CharIndex] != false) {
+      ChoiceCat = CharIndex;
+      const target = document.getElementById("GameStartBtn");
+      target.disabled = false;
+      _BuyBtn.style.display = "none";
+    } else {
+      ChoiceCat = -1;
+      const target = document.getElementById("GameStartBtn");
+      target.disabled = true;
+      _BuyBtn.style.display = "inline-block";
+    }
   }
 }
 
 function SlideRight() {
-  if (CharIndex < 4) {
+  if (CharIndex < 6) {
     ++CharIndex;
-    _SlideList.style.left = -CharIndex * 760 + "px";
-    if (LocalData.Cat[CharIndex] != false) ChoiceCat = CharIndex;
-    else ChoiceCat = -1;
+    _SlideList.style.left = -CharIndex * 190 + "px";
+    if (LocalData.Cat[CharIndex] != false) {
+      ChoiceCat = CharIndex;
+      const target = document.getElementById("GameStartBtn");
+      target.disabled = false;
+      _BuyBtn.style.display = "none";
+    } else {
+      ChoiceCat = -1;
+      const target = document.getElementById("GameStartBtn");
+      target.disabled = true;
+      _BuyBtn.style.display = "inline-block";
+    }
   }
 }
 function BuyChar() {
   if (LocalData.Coin >= 100) {
     LocalData.Coin -= 100;
-    LocalData.Cat[this.id] = true;
-    ChoiceCat = this.id;
+    LocalData.Cat[CharIndex] = true;
+    ChoiceCat = CharIndex;
+
+    _CanCount.textContent = "x" + LocalData.Coin;
 
     this.style.display = "none";
+    const target = document.getElementById("GameStartBtn");
+    target.disabled = false;
     SaveData();
     // 코인 차감
     // Buybtn 비활성
@@ -188,13 +222,22 @@ function BuyChar() {
 
 function MapLevel() {
   if (ChoiceLevel === 0) {
-    this.textContent = "하드 모드";
+    this.style.background =
+      "url('images/ui/MapLevelHard.png') no-repeat center";
+    this.style.backgroundSize = "contain";
+    this.textContent = "HARD";
     ++ChoiceLevel;
   } else if (ChoiceLevel === 1) {
-    this.textContent = "지옥 모드";
+    this.style.background =
+      "url('images/ui/MapLevelHell.png') no-repeat center";
+    this.style.backgroundSize = "contain";
+    this.textContent = "HELL";
     ++ChoiceLevel;
   } else {
-    this.textContent = "일반 모드";
+    this.style.background =
+      "url('images/ui/MapLevelNormal.png') no-repeat center";
+    this.style.backgroundSize = "contain";
+    this.textContent = "NORMAL";
     ChoiceLevel = 0;
   }
 }
