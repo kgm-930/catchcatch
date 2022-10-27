773a0148fbdaff101299d04165c3b4b26ebfcef9
YongUk <97578408+Ingyeon@users.noreply.github.com>
2022-10-27 14:23:00 +0900

    #2 :sparkles: : 몬스터 종류 구현 및 보스 초기 제작

--- catchcatch/source/JS/GAME/GameObj/player.js
+++ catchcatch/source/JS/GAME/GameObj/player.js
@@ -86,12 +86,9 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
   hitPlayer(player, alien) {
     if (player.invincible == false){
       player.invincible = true
-      alien.hp -= 1
       player.health-=1;
       console.log(player.invincible);
       console.log(player.health);
-      // 일단 피해 준 몬스터는 사라지는데 추후 코드로 몇초간 안보이게 또는 유저 잠시 무적으로 수정해야함
-      // alien.destroy();
       // 피해 1 줌
       // stop_game -= 1;
       if (player.health <= 0) {
