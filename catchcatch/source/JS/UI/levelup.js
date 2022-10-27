bb366faf47caf14c3fc3657bc2f85b3ce562f468
최영선 <vnddns26@naver.com>
2022-10-27 15:33:59 +0900

    #1 :sparkles: levelup UI 적용

--- catchcatch/source/JS/UI/levelup.js
+++ catchcatch/source/JS/UI/levelup.js
@@ -1,18 +1,19 @@
 const propertyArr = ["common", "wizard", "reaper", "ninja", "slime", "witch"];
-const commonArr = ["health", "dmgmulLevel", "heal", "speed"];
+const commonArr = ["health", "dmgmul", "heal", "speed"];
 export default function levelup() {
   const property = {
     common: {
+      name: "공통",
       health: { level: player.healthLevel, text: "최대 체력 증가" },
-      dmgmulLevel: { level: player.dmgmulLevel, text: "공격력 증가" },
+      dmgmul: { level: player.dmgmulLevel, text: "공격력 증가" },
       heal: { level: player.healLevel, text: "회복력 증가" },
       speed: { level: player.speedLevel, text: "이동속도 증가" },
     },
-    wizard: { fairy: wizard },
-    reaper: { fairy: reaper },
-    ninja: { fairy: ninja },
-    slime: { fairy: slime },
-    witch: { fairy: witch },
+    wizard: { name: "위저드", fairy: wizard, text: ["ㅇ", "", "", ""] },
+    reaper: { name: "사신", fairy: reaper, text: ["ㄹ", "", "", ""] },
+    ninja: { name: "닌자", fairy: ninja, text: ["ㅇ", "", "", ""] },
+    slime: { name: "슬라임", fairy: slime, text: ["ㅇ", "", "", ""] },
+    witch: { name: "마녀", fairy: witch, text: ["ㅇ", "", "", ""] },
   };
   let randomIndexArray = [];
   for (let i = 0; i < 3; i++) {
@@ -25,10 +26,16 @@ export default function levelup() {
   }
   console.log(randomIndexArray);
   const gameContainer = document.querySelector("#game-container");
+
   // const levelupContainer = document.createElement("div");
   // levelupContainer.setAttribute("class", "levelupContainer");
   $this.pause();
   const levelupContainer = document.createElement("div");
+  levelupContainer.style.backgroundImage =
+    'url("../images/ui/levelup/Base.png")';
+  levelupContainer.style.backgroundPosition = "center";
+  levelupContainer.style.backgroundRepeat = "no-repeat";
+  levelupContainer.style.backgroundSize = "cotain";
   levelupContainer.setAttribute("class", "levelupContainer");
   const randomCommon = Math.floor(Math.random() * 4);
   for (let i = 0; i < 3; i++) {
@@ -38,18 +45,51 @@ export default function levelup() {
     levelupContent.setAttribute("class", "levelupContent");
     levelupContent.setAttribute("id", `${propertyArr[randomIndexArray[i]]}`);
     levelupImg.setAttribute("class", "levelupImg");
-    levelupImg.innerText = `${propertyArr[randomIndexArray[i]]}`;
     levelupText.setAttribute("class", "levelupText");
+    const levelImgandNameBox = document.createElement("div");
+    levelImgandNameBox.setAttribute("class", "levelImgandNameBox");
+    const levelupName = document.createElement("div");
+    levelupName.setAttribute("class", "levelupName");
+
     if (propertyArr[randomIndexArray[i]] !== "common") {
       levelupText.innerText = `${
-        property[propertyArr[randomIndexArray[i]]].fairy.level
+        property[propertyArr[randomIndexArray[i]]].text[
+          property[propertyArr[randomIndexArray[i]]].fairy.level - 1
+        ]
       }`;
+      levelupContent.style.backgroundImage =
+        'url("../images/ui/levelup/fairyupgrade_addName.png")';
+      // 설명인데..
+      levelupName.innerHTML = `[${
+        property[propertyArr[randomIndexArray[i]]].name
+      }] <br> Lv. ${property[propertyArr[randomIndexArray[i]]].fairy.level}`;
+      // levelupName.textContent += "Lv. 1";
     } else {
+      levelupContent.style.backgroundImage =
+        'url("../images/ui/levelup/commonupgrade_addName.png")';
+
       levelupText.innerText =
         property.common[commonArr[randomCommon]].level +
         property.common[commonArr[randomCommon]].text;
+      levelupName.innerHTML = `[공통] <br> Lv. ${
+        property.common[commonArr[randomCommon]].level
+      }`;
+      // levelupName.textContent += "Lv. 1";
     }
-    levelupContent.appendChild(levelupImg);
+    levelupContent.style.backgroundRepeat = "no-repeat";
+    levelupContent.style.backgroundPosition = "center";
+    levelupContent.style.backgroundSize = "contain";
+
+    //스킬 아이콘
+    levelupImg.style.backgroundImage = 'url("../images/ui/Icon/sample.png")';
+
+    levelupImg.style.backgroundRepeat = "no-repeat";
+    levelupImg.style.backgroundPosition = "center";
+    levelupImg.style.backgroundSize = "contain";
+
+    levelImgandNameBox.appendChild(levelupImg);
+    levelImgandNameBox.appendChild(levelupName);
+    levelupContent.appendChild(levelImgandNameBox);
     levelupContent.appendChild(levelupText);
     levelupContainer.appendChild(levelupContent);
   }
