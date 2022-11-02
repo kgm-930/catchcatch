0363fb71245ba678d2d81cbf0cadffda061c7152
kgm <72342542+kgm-930@users.noreply.github.com>
2022-11-02 11:57:33 +0900

    #1 :sparkles: 각종 코드 수정

--- catchcatch/source/JS/GAME/GameObj/player.js
+++ catchcatch/source/JS/GAME/GameObj/player.js
@@ -1,188 +1,187 @@
-import { cursors, mapSize } from "../game.js";
-import { gameover, updateExp } from "../../UI/inGameUI.js";
+import {cursors, mapSize} from "../game.js";
+import {GameOver, updateExp} from "../../UI/inGameUI.js";
 import levelup from "../../UI/levelup.js";
+
 export const Direction = Object.freeze({
-  Up: "Up",
-  Down: "Down",
-  Left: "Left",
-  Right: "Right",
+    Up: "Up",
+    Down: "Down",
+    Left: "Left",
+    Right: "Right",
 });
 
 export default class Player extends Phaser.Physics.Arcade.Sprite {
-  maxHealth = 20;
-  health = 20;
-  healthLevel = 1;
-  dmgmul = 1;
-  dmgmulLevel = 1;
-  speed = 100;
-  speedLevel = 1;
-  maxExp = 3;
-  exp = 0;
-  level = 1;
-  maxExpBonus = 1;
-  coin = 1000;
-  // 캐릭터 특수능력 일단 보류
-  ablity;
-  heal = 0;
-  healCount = 0;
-  maxHealCount = 300;
-  healLevel = 1;
-  fairy;
-  invincible = false;
-  constructor(scene, dmgmul, maxHealth, health, catname) {
-    super(scene, 0, 0, catname);
-    this.alpha = 1;
-    this.dmgmul = dmgmul;
-    this.maxHealth = maxHealth;
-    this.health = health;
-    scene.add.existing(this);
-    scene.physics.add.existing(this);
-    scene.anims.create({
-      key: "turn",
-      frames: this.anims.generateFrameNumbers(catname, { start: 0, end: 0 }),
-      frameRate: 10,
-    });
-    scene.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers(catname, { start: 1, end: 7 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    scene.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers(catname, { start: 1, end: 7 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-  }
+    maxHealth = 20;
+    health = 20;
+    healthLevel = 1;
+    dmgMul = 1;
+    dmgMulLevel = 1;
+    speed = 100;
+    speedLevel = 1;
+    maxExp = 3;
+    exp = 0;
+    level = 1;
+    maxExpBonus = 1;
+    coin = 1000;
+    // 캐릭터 특수능력 일단 보류
+    //ability;
+    heal = 0;
+    healCount = 0;
+    maxHealCount = 300;
+    healLevel = 1;
+    fairy;
+    invincible = false;
 
-  expUp() {
-    this.exp++;
-    this.expUpdate();
-    updateExp();
-  }
-
-  commonLevelUp(id) {
-    switch (id) {
-      case "dmgmul":
-        this.dmgmulLevel++;
-        this.dmgmul += 0.1;
-        break;
-      case "health":
-        this.healthLevel++;
-        this.maxHealth += 5;
-        this.health += 5;
-        break;
-      case "heal":
-        this.healLevel++;
-        this.heal += 1;
-        break;
-      case "speed":
-        this.speedLevel++;
-        this.speed += 10;
-        break;
+    constructor(scene, dmgMul, maxHealth, health, catName) {
+        super(scene, 0, 0, catName);
+        this.alpha = 1;
+        this.dmgMul = dmgMul;
+        this.maxHealth = maxHealth;
+        this.health = health;
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+        scene.anims.create({
+            key: "turn",
+            frames: this.anims.generateFrameNumbers(catName, {start: 0, end: 0}),
+            frameRate: 10,
+        });
+        scene.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers(catName, {start: 1, end: 7}),
+            frameRate: 10,
+            repeat: -1,
+        });
+        scene.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers(catName, {start: 1, end: 7}),
+            frameRate: 10,
+            repeat: -1,
+        });
     }
-  }
 
-  expUpdate() {
-    if (this.exp >= this.maxExp) {
-      if (!isLevelup) {
-        this.exp -= this.maxExp;
-        this.maxExp += this.maxExpBonus;
-        this.level++;
-        isLevelup = true;
-        levelup();
+    expUp() {
+        this.exp++;
+        this.expUpdate();
         updateExp();
-      }
-    }
-    updateExp();
-  }
-
-  changeFairy(fairy) {
-    this.fairy = fairy;
-  }
-
-  move(direction) {
-    this.fairy.x = this.x - 20;
-    this.fairy.y = this.y - 50;
-    
-    if (cursors.left.isDown) {
-      this.setVelocityX(-this.speed);
-      this.anims.play("left", true);
-      this.flipX = true;
-    } else if (cursors.right.isDown) {
-      this.setVelocityX(this.speed);
-      this.flipX = false;
-      this.anims.play("right", true);
-    } else {
-      this.setVelocityX(0);
     }
 
-    if (cursors.up.isDown) {
-      this.setVelocityY(-this.speed);
-
-      if (cursors.left.isDown) {
-        this.anims.play("left", true);
-      } else {
-        this.anims.play("right", true);
-      }
-    } else if (cursors.down.isDown) {
-      this.setVelocityY(this.speed);
-
-      if (cursors.left.isDown) {
-        this.anims.play("left", true);
-      } else {
-        this.anims.play("right", true);
-      }
-    } else {
-      this.setVelocityY(0);
-      if (!cursors.left.isDown && !cursors.right.isDown) {
-        this.anims.play("turn", true);
-      }
-    }
-    let speedDiag = this.speed * (1 / 1.44);
-    if (cursors.left.isDown && cursors.up.isDown)
-    {
-        this.body.setVelocityX(-speedDiag);
-        this.body.setVelocityY(-speedDiag);
-    }
-    
-    // Up and right
-    if (cursors.right.isDown && cursors.up.isDown)
-    {
-        this.body.setVelocityX(speedDiag);
-        this.body.setVelocityY(-speedDiag);
-    }
-    
-    // Down and right
-    if (cursors.right.isDown && cursors.down.isDown)
-    {
-        this.body.setVelocityX(speedDiag);
-        this.body.setVelocityY(speedDiag);
-    }
-    
-    // Down and left
-    if (cursors.left.isDown && cursors.down.isDown)
-    {
-        this.body.setVelocityX(-speedDiag);
-        this.body.setVelocityY(speedDiag);
+    commonLevelUp(id) {
+        switch (id) {
+            case "dmgMul":
+                this.dmgMulLevel++;
+                this.dmgMul += 0.1;
+                break;
+            case "health":
+                this.healthLevel++;
+                this.maxHealth += 5;
+                this.health += 5;
+                break;
+            case "heal":
+                this.healLevel++;
+                this.heal += 1;
+                break;
+            case "speed":
+                this.speedLevel++;
+                this.speed += 10;
+                break;
+        }
     }
 
-  }
-
-  hitByEnemy(damage) {}
-
-  hitPlayer(player, alien) {
-    if (player.invincible == false) {
-      player.invincible = true;
-      player.body.checkCollision.none = true;
-      player.health -= 1;
-      // 피해 1 줌
-      // stop_game -= 1;
-      if (player.health <= 0) {
-        gameover();
-        $this.pause();
-      }
+    expUpdate() {
+        if (this.exp >= this.maxExp) {
+            if (!isLevelup) {
+                this.exp -= this.maxExp;
+                this.maxExp += this.maxExpBonus;
+                this.level++;
+                isLevelup = true;
+                levelup();
+                updateExp();
+            }
+        }
+        updateExp();
     }
-  }
+
+    changeFairy(fairy) {
+        this.fairy = fairy;
+    }
+
+    move(direction) {
+        this.fairy.x = this.x - 20;
+        this.fairy.y = this.y - 50;
+
+        if (cursors.left.isDown) {
+            this.setVelocityX(-this.speed);
+            this.anims.play("left", true);
+            this.flipX = true;
+        } else if (cursors.right.isDown) {
+            this.setVelocityX(this.speed);
+            this.flipX = false;
+            this.anims.play("right", true);
+        } else {
+            this.setVelocityX(0);
+        }
+
+        if (cursors.up.isDown) {
+            this.setVelocityY(-this.speed);
+
+            if (cursors.left.isDown) {
+                this.anims.play("left", true);
+            } else {
+                this.anims.play("right", true);
+            }
+        } else if (cursors.down.isDown) {
+            this.setVelocityY(this.speed);
+
+            if (cursors.left.isDown) {
+                this.anims.play("left", true);
+            } else {
+                this.anims.play("right", true);
+            }
+        } else {
+            this.setVelocityY(0);
+            if (!cursors.left.isDown && !cursors.right.isDown) {
+                this.anims.play("turn", true);
+            }
+        }
+        let speedDiag = this.speed * (1 / 1.44);
+        if (cursors.left.isDown && cursors.up.isDown) {
+            this.body.setVelocityX(-speedDiag);
+            this.body.setVelocityY(-speedDiag);
+        }
+
+        // Up and right
+        if (cursors.right.isDown && cursors.up.isDown) {
+            this.body.setVelocityX(speedDiag);
+            this.body.setVelocityY(-speedDiag);
+        }
+
+        // Down and right
+        if (cursors.right.isDown && cursors.down.isDown) {
+            this.body.setVelocityX(speedDiag);
+            this.body.setVelocityY(speedDiag);
+        }
+
+        // Down and left
+        if (cursors.left.isDown && cursors.down.isDown) {
+            this.body.setVelocityX(-speedDiag);
+            this.body.setVelocityY(speedDiag);
+        }
+
+    }
+
+    hitByEnemy(damage) {
+    }
+
+    hitPlayer(player) {
+        if (player.invincible === false) {
+            player.invincible = true;
+            player.body.checkCollision.none = true;
+            player.health -= 1;
+            // 피해 1 줌
+            // stop_game -= 1;
+            if (player.health <= 0) {
+                GameOver();
+                $this.pause();
+            }
+        }
+    }
 }
--- catchcatch/source/JS/UI/inGameUI.js
+++ catchcatch/source/JS/UI/inGameUI.js
@@ -1,185 +1,186 @@
 import "../../CSS/UI/inGameUI.css";
-import { CharSpaceOn } from "./CharSpace.js";
+import {CharSpaceOn} from "./CharSpace.js";
 
-let Timertxt = document.createElement("div");
-let Minute = 0;
-let Second = 0;
+let timerTxt = document.createElement("div");
+let minute = 0;
+let second = 0;
 
-let _catcoin;
+let catCoin;
 
 export default function inGameUI() {
-  const gameContainer = document.querySelector("#game-container");
-  // const progress = document.createElement("progress");
+    const gameContainer = document.querySelector("#game-container");
+    // const progress = document.createElement("progress");
 
-  const holeHP = document.createElement("div");
-  holeHP.setAttribute("class", "holeHP");
-  const hp = document.createElement("div");
-  hp.setAttribute("class", "hp");
-  hp.innerText = hole.hp;
-  holeHP.appendChild(hp);
-  gameContainer.appendChild(holeHP);
+    const holeHp = document.createElement("div");
+    holeHp.setAttribute("class", "holeHp");
+    const hp = document.createElement("div");
+    hp.setAttribute("class", "hp");
+    hp.innerText = hole.hp;
+    holeHp.appendChild(hp);
+    gameContainer.appendChild(holeHp);
 
-  _catcoin = document.createElement("div");
-  _catcoin.setAttribute("class", "catcoin");
-  _catcoin.setAttribute("id", "catcoin");
-  _catcoin.textContent = global.coin + " Coin";
+    catCoin = document.createElement("div");
+    catCoin.setAttribute("class", "catCoin");
+    catCoin.setAttribute("id", "catCoin");
+    catCoin.textContent = global.coin + " Coin";
 
-  const stats = document.createElement("div");
-  const heal = document.createElement("div");
-  const healtxt = document.createElement("div");
+    const stats = document.createElement("div");
+    const heal = document.createElement("div");
+    const healTxt = document.createElement("div");
 
-  const dmgmul = document.createElement("div");
-  const dmgmultxt = document.createElement("div");
+    const dmgMul = document.createElement("div");
+    const dmgMulTxt = document.createElement("div");
 
-  const speed = document.createElement("div");
-  const speedtxt = document.createElement("div");
+    const speed = document.createElement("div");
+    const speedTxt = document.createElement("div");
 
-  Timertxt.setAttribute("class", "Timer");
-  gameContainer.appendChild(Timertxt);
-  Minute = 0;
-  Second = 0;
+    timerTxt.setAttribute("class", "Timer");
+    gameContainer.appendChild(timerTxt);
+    minute = 0;
+    second = 0;
 
-  Timertxt.textContent = `${Minute}:${Second}`;
+    timerTxt.textContent = `${minute}:${second}`;
 
-  // progress.setAttribute("id", "progress");
-  // progress.setAttribute("value", player.exp);
-  // progress.setAttribute("max", 100);
-  stats.setAttribute("class", "stats");
-  heal.setAttribute("class", "stat");
-  healtxt.setAttribute("class", "healtxt");
+    // progress.setAttribute("id", "progress");
+    // progress.setAttribute("value", player.exp);
+    // progress.setAttribute("max", 100);
+    stats.setAttribute("class", "stats");
+    heal.setAttribute("class", "stat");
+    healTxt.setAttribute("class", "healTxt");
 
-  dmgmul.setAttribute("class", "stat");
-  dmgmultxt.setAttribute("class", "dmgmultxt");
+    dmgMul.setAttribute("class", "stat");
+    dmgMulTxt.setAttribute("class", "dmgMulTxt");
 
-  speed.setAttribute("class", "stat");
-  speedtxt.setAttribute("class", "speedtxt");
+    speed.setAttribute("class", "stat");
+    speedTxt.setAttribute("class", "speedTxt");
 
-  heal.setAttribute("id", "heal");
-  heal.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  heal.style.backgroundPosition = "center";
-  heal.style.backgroundRepeat = "no-repeat";
-  heal.style.backgroundSize = "contain";
+    heal.setAttribute("id", "heal");
+    heal.style.backgroundImage = "url('images/ui/Icon/sample.png')";
+    heal.style.backgroundPosition = "center";
+    heal.style.backgroundRepeat = "no-repeat";
+    heal.style.backgroundSize = "contain";
 
-  dmgmul.setAttribute("id", "dmgmul");
-  dmgmul.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  dmgmul.style.backgroundPosition = "center";
-  dmgmul.style.backgroundRepeat = "no-repeat";
-  dmgmul.style.backgroundSize = "contain";
+    dmgMul.setAttribute("id", "dmgMul");
+    dmgMul.style.backgroundImage = "url('images/ui/Icon/sample.png')";
+    dmgMul.style.backgroundPosition = "center";
+    dmgMul.style.backgroundRepeat = "no-repeat";
+    dmgMul.style.backgroundSize = "contain";
 
-  speed.setAttribute("id", "speed");
-  speed.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  speed.style.backgroundPosition = "center";
-  speed.style.backgroundRepeat = "no-repeat";
-  speed.style.backgroundSize = "contain";
+    speed.setAttribute("id", "speed");
+    speed.style.backgroundImage = "url('images/ui/Icon/sample.png')";
+    speed.style.backgroundPosition = "center";
+    speed.style.backgroundRepeat = "no-repeat";
+    speed.style.backgroundSize = "contain";
 
-  healtxt.innerText = `Lv.${player.healLevel}`;
-  heal.appendChild(healtxt);
-  dmgmultxt.innerText = `Lv.${player.dmgmulLevel}`;
-  dmgmul.appendChild(dmgmultxt);
-  speedtxt.innerText = `Lv.${player.speedLevel}`;
-  speed.appendChild(speedtxt);
+    healTxt.innerText = `Lv.${player.healLevel}`;
+    heal.appendChild(healTxt);
+    dmgMulTxt.innerText = `Lv.${player.dmgMulLevel}`;
+    dmgMul.appendChild(dmgMulTxt);
+    speedTxt.innerText = `Lv.${player.speedLevel}`;
+    speed.appendChild(speedTxt);
 
-  stats.appendChild(_catcoin);
-  stats.appendChild(heal);
-  stats.appendChild(dmgmul);
-  stats.appendChild(speed);
-  // gameContainer.appendChild(progress);
-  gameContainer.appendChild(stats);
+    stats.appendChild(catCoin);
+    stats.appendChild(heal);
+    stats.appendChild(dmgMul);
+    stats.appendChild(speed);
+    // gameContainer.appendChild(progress);
+    gameContainer.appendChild(stats);
 }
 
 export function updateExp() {
-  // const progress = document.querySelector("#progress");
-  const heal = document.querySelector(".healtxt");
-  const dmgmul = document.querySelector(".dmgmultxt");
-  const speed = document.querySelector(".speedtxt");
-  heal.innerText = `Lv.${player.healLevel}`;
-  dmgmul.innerText = `Lv.${player.dmgmulLevel}`;
-  speed.innerText = `Lv.${player.speedLevel} `;
-  // progress.setAttribute("value", player.exp);
+    // const progress = document.querySelector("#progress");
+    const heal = document.querySelector(".healTxt");
+    const dmgMul = document.querySelector(".dmgMulTxt");
+    const speed = document.querySelector(".speedTxt");
+    heal.innerText = `Lv.${player.healLevel}`;
+    dmgMul.innerText = `Lv.${player.dmgMulLevel}`;
+    speed.innerText = `Lv.${player.speedLevel} `;
+    // progress.setAttribute("value", player.exp);
 }
 
 export function updateHP() {
-  // console.log(hole.hp);
-  const holeHP = document.querySelector(".holeHP");
-  const hp = document.querySelector(".hp");
-  hp.innerText = hole.hp;
+    // console.log(hole.hp);
+    const holeHp = document.querySelector(".holeHp");
+    const hp = document.querySelector(".hp");
+    hp.innerText = hole.hp;
 }
 
-export function gameover() {
-  const gameContainer = document.querySelector("#game-container");
-  const gameoverContainer = document.createElement("div");
-  gameoverContainer.setAttribute("class", "gameoverContainer");
-  const gameover = document.createElement("div");
-  gameover.setAttribute("class", "gameover");
-  gameover.innerText = "GAME OVER";
-  const again = document.createElement("div");
-  again.innerText = "다시하기";
-  again.setAttribute("class", "again");
-  again.addEventListener("click", () => {
-    // gameContainer.innerHTML = "";
-    // gameContainer.style.display = "none";
-    // const startPage = document.querySelector(".StartPage");
-    // startPage.style.display = "flex";
-    // gameContainer.removeChild(gameoverContainer);
-    // CharSpaceOn();
-    // $this.restart();
-    window.location.reload();
-  });
-  gameoverContainer.appendChild(gameover);
-  gameoverContainer.appendChild(again);
-  gameContainer.appendChild(gameoverContainer);
+export function GameOver() {
+    const gameContainer = document.querySelector("#game-container");
+    const gameOverContainer = document.createElement("div");
+    gameOverContainer.setAttribute("class", "gameOverContainer");
+    const gameOver = document.createElement("div");
+    gameOver.setAttribute("class", "gameOver");
+    gameOver.innerText = "GAME OVER";
+    const again = document.createElement("div");
+    again.innerText = "다시하기";
+    again.setAttribute("class", "again");
+    again.addEventListener("click", () => {
+        // gameContainer.innerHTML = "";
+        // gameContainer.style.display = "none";
+        // const startPage = document.querySelector(".StartPage");
+        // startPage.style.display = "flex";
+        // gameContainer.removeChild(gameoverContainer);
+        // CharSpaceOn();
+        // $this.restart();
+        window.location.reload();
+    });
+    gameOverContainer.appendChild(gameOver);
+    gameOverContainer.appendChild(again);
+    gameContainer.appendChild(gameOverContainer);
 }
-export function Updatetimer() {
-  if (global.gameTimer != 0 && global.gameTimer % 60 === 0) {
-    ++Second;
-    if (Second === 60) {
-      ++Minute;
-      Second = 0;
+
+export function UpdateTimer() {
+    if (global.gameTimer !== 0 && global.gameTimer % 60 === 0) {
+        ++second;
+        if (second === 60) {
+            ++minute;
+            second = 0;
+        }
     }
-  }
-  if (Second < 10) {
-    if (Minute < 10) Timertxt.textContent = `0${Minute}:0${Second}`;
-    else Timertxt.textContent = `${Minute}:0${Second}`;
-  } else if (Minute < 10) {
-    if (Second < 10) Timertxt.textContent = `0${Minute}:0${Second}`;
-    else Timertxt.textContent = `0${Minute}:${Second}`;
-  } else Timertxt.textContent = `w${Minute}:${Second}`;
+    if (second < 10) {
+        if (minute < 10) timerTxt.textContent = `0${minute}:0${second}`;
+        else timerTxt.textContent = `${minute}:0${second}`;
+    } else if (minute < 10) {
+        if (second < 10) timerTxt.textContent = `0${minute}:0${second}`;
+        else timerTxt.textContent = `0${minute}:${second}`;
+    } else timerTxt.textContent = `w${minute}:${second}`;
 
-  if (Minute === 20) {
-    $this.pause();
-    GameClear();
-    //게임클리어
-  }
+    if (minute === 20) {
+        $this.pause();
+        GameClear();
+        //게임클리어
+    }
 }
 
 function GameClear() {
-  // 게임 중지
-  const gameContainer = document.querySelector("#game-container");
+    // 게임 중지
+    const gameContainer = document.querySelector("#game-container");
 
-  const GameClearSpace = document.createElement("div");
-  GameClearSpace.setAttribute("class", "GameClearSpace");
+    const GameClearSpace = document.createElement("div");
+    GameClearSpace.setAttribute("class", "GameClearSpace");
 
-  const ClearText = document.createElement("div");
-  ClearText.setAttribute("class", "ClearText");
-  ClearText.textContent = "GameClear!";
+    const ClearText = document.createElement("div");
+    ClearText.setAttribute("class", "ClearText");
+    ClearText.textContent = "GameClear!";
 
-  GameClearSpace.appendChild(ClearText);
+    GameClearSpace.appendChild(ClearText);
 
-  const GoHomeBtn = document.createElement("button");
-  GoHomeBtn.setAttribute("class", "GoHomeBtn");
-  GoHomeBtn.textContent = "홈으로";
+    const GoHomeBtn = document.createElement("button");
+    GoHomeBtn.setAttribute("class", "GoHomeBtn");
+    GoHomeBtn.textContent = "홈으로";
 
-  GoHomeBtn.addEventListener("click", GoHome);
+    GoHomeBtn.addEventListener("click", GoHome);
 
-  GameClearSpace.appendChild(GoHomeBtn);
+    GameClearSpace.appendChild(GoHomeBtn);
 
-  gameContainer.appendChild(GameClearSpace);
+    gameContainer.appendChild(GameClearSpace);
 }
 
 function GoHome() {
-  window.location.reload();
+    window.location.reload();
 }
 
 export function UpdateCatCoin() {
-  _catcoin.textContent = global.coin + " Coin";
+    catCoin.textContent = global.coin + " Coin";
 }
