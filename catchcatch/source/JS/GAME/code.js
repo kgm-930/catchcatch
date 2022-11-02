80462d71453a5cde1339b1b7f303992faced7b0e
h5282001 <h5282001@naver.com>
2022-11-02 14:26:07 +0900

    :sparkles: 코딩모드 연결

---
+++ catchcatch/source/JS/GAME/code.js
@@ -0,0 +1,218 @@
+import Player from "./CodeObj/player.js";
+import {Chunk, Tile} from "./entities.js";
+
+export const codeConfig = {
+  type: Phaser.AUTO,
+  width: 600,
+  height: 600,
+  parent: "game-container",
+  backgroundColor: "black",
+  resolution: window.devicePixelRatio,
+  pixelArt: true,
+  roundPixels: true,
+  scene: {
+    //scene 제어에
+    preload: preload,
+    create: create,
+    update: update,
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      fps: 20,
+      debug: false,
+      fixedStep: false,
+    },
+  },
+};
+
+let cats;
+var player = "";
+global.thisScene = "";
+var gameOver = false;
+var scoreText;
+global.gameTimer = 0;
+
+var map;
+var chunks = [];
+export var camera;
+let frameTime = 0;
+// 몬스터 변수 선언
+export var monsterSet;
+var monster;
+
+function preload() {
+  //map start
+  this.load.image("sprWater", "images/map/sprWater.png");
+  this.load.image("sprSand", "images/map/sprSand.png");
+  this.load.image("sprGrass", "images/map/sprGrass.png");
+  //map end
+
+  //player start
+    // 플레이어 스프라이트
+    this.load.spritesheet("cat1", "images/cat/cat1.png", {
+        frameWidth: 96,
+        frameHeight: 100,
+    });
+  //
+}
+
+function create() {
+  //player start
+  player = new Player(this,10,10,"cat1");
+  player.setDepth(3);
+  //player end
+  thisScene = this;
+  //map start
+  this.chunkSize = 8;
+  this.tileSize = 300;
+  this.cameraSpeed = 1;
+
+  this.cameras.main.setZoom(1);
+  this.followPoint = new Phaser.Math.Vector2(
+    this.cameras.main.worldView.x + this.cameras.main.worldView.width * 0.5,
+    this.cameras.main.worldView.y + this.cameras.main.worldView.height * 0.5
+  );
+
+  global.$this = this.scene;
+
+  camera = this.cameras.main;
+
+  //map start
+  var snappedChunkX =
+    this.chunkSize *
+    this.tileSize *
+    Math.round(this.followPoint.x / (this.chunkSize * this.tileSize));
+  var snappedChunkY =
+    this.chunkSize *
+    this.tileSize *
+    Math.round(this.followPoint.y / (this.chunkSize * this.tileSize));
+
+  snappedChunkX = snappedChunkX / this.chunkSize / this.tileSize;
+  snappedChunkY = snappedChunkY / this.chunkSize / this.tileSize;
+
+  for (var x = snappedChunkX - 2; x < snappedChunkX + 2; x++) {
+    for (var y = snappedChunkY - 2; y < snappedChunkY + 2; y++) {
+      var existingChunk = getChunk(x, y);
+
+      if (existingChunk == null) {
+        var newChunk = new Chunk(this, x, y);
+        chunks.push(newChunk);
+      }
+    }
+  }
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+
+    if (
+      Phaser.Math.Distance.Between(
+        snappedChunkX,
+        snappedChunkY,
+        chunk.x,
+        chunk.y
+      ) < 3
+    ) {
+      if (chunk !== null) {
+        chunk.load();
+      }
+    } else {
+      if (chunk !== null) {
+        chunk.unload();
+      }
+    }
+  }
+
+  for (var i = 0; i < chunks.length; i++) {
+    var chunk = chunks[i];
+
+    if (
+      Phaser.Math.Distance.Between(
+        snappedChunkX,
+        snappedChunkY,
+        chunk.x,
+        chunk.y
+      ) < 3
+    ) {
+      if (chunk !== null) {
+        chunk.load();
+      }
+    } else {
+      if (chunk !== null) {
+        chunk.unload();
+      }
+    }
+  }
+
+  this.cameras.main.centerOn(this.followPoint.x, this.followPoint.y);
+  //map enderlap(magics, monsterSet, attack);
+
+    var snappedChunkX =
+      this.chunkSize *
+      this.tileSize *
+      Math.round(this.followPoint.x / (this.chunkSize * this.tileSize));
+    var snappedChunkY =
+      this.chunkSize *
+      this.tileSize *
+      Math.round(this.followPoint.y / (this.chunkSize * this.tileSize));
+
+    snappedChunkX = snappedChunkX / this.chunkSize / this.tileSize;
+    snappedChunkY = snappedChunkY / this.chunkSize / this.tileSize;
+
+    for (var x = snappedChunkX - 2; x < snappedChunkX + 2; x++) {
+      for (var y = snappedChunkY - 2; y < snappedChunkY + 2; y++) {
+        var existingChunk = getChunk(x, y);
+
+        if (existingChunk == null) {
+          var newChunk = new Chunk(this, x, y);
+          chunks.push(newChunk);
+        }
+      }
+    }
+    for (var i = 0; i < chunks.length; i++) {
+      var chunk = chunks[i];
+
+      if (
+        Phaser.Math.Distance.Between(
+          snappedChunkX,
+          snappedChunkY,
+          chunk.x,
+          chunk.y
+        ) < 3
+      ) {
+        if (chunk !== null) {
+          chunk.load();
+        }
+      } else {
+        if (chunk !== null) {
+          chunk.unload();
+        }
+      }
+    }
+
+    this.followPoint.x = player.x;
+    this.followPoint.y = player.y;
+    //map end
+
+
+this.cameras.main.setZoom(1);
+this.cameras.main.startFollow(player, false);
+console.log(this.cameras);
+}
+
+
+function update(){
+
+}
+
+//map start
+function getChunk(x, y) {
+  var chunk = null;
+  for (var i = 0; i < chunks.length; i++) {
+    if (chunks[i].x == x && chunks[i].y == y) {
+      chunk = chunks[i];
+    }
+  }
+  return chunk;
+}
+
+//map end
