4165b355241114f01c3ca1ae669fa27c7a4b8c68
YongUk <97578408+Ingyeon@users.noreply.github.com>
2022-11-03 14:40:55 +0900

    #2 :sparkles:  몬스터 스프라이트 적용 1차

--- catchcatch/source/JS/GAME/GameObj/explosion.js
+++ catchcatch/source/JS/GAME/GameObj/explosion.js
@@ -1,7 +1,7 @@
 export default class Explosion extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y) {
         // magic5_1를 나중에 폭발 스프라이트 애니메이션  key로 바꾸기
-        super(scene, x, y, "magic5_1");
+        super(scene, x, y, "monster_die");
         scene.time.addEvent({
             delay: 500, callback: () => {
                 this.destroy();
@@ -10,6 +10,6 @@ export default class Explosion extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);
         this.scale = 2;
         UICam.ignore(this);
-        this.play("magic5_1");
+        this.play("monster_die");
     }
 }
