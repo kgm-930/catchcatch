bb8c7c27faf58fdefa9188e7e2a1e49da6093d88
h5282001 <h5282001@naver.com>
2022-11-04 15:05:28 +0900

    #7 :sparkles: 코딩모드 완성 직전

--- catchcatch/source/JS/GAME/CodeObj/enemy.js
+++ catchcatch/source/JS/GAME/CodeObj/enemy.js
@@ -1,7 +1,6 @@
 import Explosion from "./explosion";
 
 export default class Enemy extends Phaser.Physics.Arcade.Sprite {
-
   maxHealth = 3;
   health = 3;
   velo;
@@ -9,67 +8,86 @@ export default class Enemy extends Phaser.Physics.Arcade.Sprite {
   type;
   monSpiece;
   constructor(scene, velo, randomX, randomY, monSpiece, anim, type) {
-    scene.time.addEvent({ delay: 400, callback: () => { if(this.active===true){this.invincible = false; this.anime()}}, loop: true });
+    scene.time.addEvent({
+      delay: 400,
+      callback: () => {
+        if (this.active === true) {
+          this.invincible = false;
+          this.anime();
+        }
+      },
+      loop: true,
+    });
     super(scene, randomX, randomY, monSpiece);
     this.velo = velo;
     this.alpha = 1;
-    this.anim = anim;
-    this.monSpiece = monSpiece;
+    switch (type) {
+      case 0:
+        let randomNum = Math.floor(Math.random() * 7 + 1);
+        this.monSpiece = "cat" + randomNum;
+        this.anim = "cat" + randomNum;
+        break;
+      case 1:
+        this.monSpiece = "alien";
+        this.anim = this.monSpiece;
+        break;
+      case 2:
+        this.monSpiece = "worm";
+        this.anim = this.monSpiece;
+        break;
+      case 3:
+        this.monSpiece = "sonic";
+        this.anim = this.monSpiece;
+        break;
+      case 4:
+        this.monSpiece = "turtle";
+        this.anim = this.monSpiece;
+        break;
+      case 5:
+        this.monSpiece = "babySlime";
+        this.anim = "slime";
+        break;
+    }
     this.type = type;
-    this.scale = 1;
+    this.scale = 0.8;
+    if (this.monSpiece === "babySlime") {
+      this.scale = 2;
+    } else if (this.monSpiece === "alien" || this.monSpiece === "alienPlus") {
+      this.scale = 2.5;
+    } else if (
+      this.monSpiece === "turtle" ||
+      this.monSpiece === "sonic" ||
+      this.monSpiece === "slime"
+    ) {
+      this.scale = 3;
+    }
     scene.add.existing(this);
     scene.physics.add.existing(this);
     this.anime();
+    scene.events.on("update", () => {
+      this.update();
+    });
   }
 
-  anime(){
-    if (this.monSpiece == 'alien'){
-      this
-      .setTint(0xFFFA00) // 노랑
+  update() {
+    if (!this.body) {
+      return;
     }
-    else if (this.monSpiece == 'alien_plus'){
-      this
-      .setTint(0xC29F6D)  // 갈색
+    if (this.body.velocity.x > 0) this.flipX = false;
+    else this.flipX = true;
+    if (this.cc === "earth") {
+      this.velocity = 0;
+    } else if (this.cc === "water") {
+      this.velocity = 10;
     }
-    else if (this.monSpiece == 'worm'){
-      this
-      .setTint(0x00ff00);} // 연두
+  }
 
-    else if (this.monSpiece == 'worm_plus'){
-      this  
-      .setTint(0xFFAAFF)}  // 핑크
-
-    else if (this.monSpiece == 'sonic'){
-      this
-      .setTint(0x0000ff)  // 파랑
-    }
-
-    else if (this.monSpiece == 'turtle'){
-      this
-      .setTint(0xac28f6) // 보라
-    }
-
-    else if (this.monSpiece == 'slime'){
-      this
-      .setTint(0x000000)  // 검정
-    }
-
-    else if (this.monSpiece == 'baby_slime'){
-      this
-      .setTint(0x000000)  // 검정
-    }
-
-    else if (this.monSpiece == 'fly'){
-      this
-      .setTint(0x00EBFF)  // 시안블루
-    }
-    this
-    .play(this.anim);
+  anime() {
+    this.clearTint().play(this.anim);
     // .setTint(Phaser.Display.Color.RandomRGB().color)
-
   }
 
-  die_anim(){
+  die_anim() {
     new Explosion(thisScene, this.x, this.y);
     // this.scene.m_explosionSound.play();  몬스터 폭발 사운드
   }
