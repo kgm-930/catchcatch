b1f80e0db43edb79e56f83b42626d77dc10dece9
h5282001 <h5282001@naver.com>
2022-11-06 03:30:57 +0900

    #3 :sparkles: vfx update

--- catchcatch/source/JS/GAME/GameObj/magic.js
+++ catchcatch/source/JS/GAME/GameObj/magic.js
@@ -20,7 +20,16 @@ export default class Magic extends Phaser.Physics.Arcade.Sprite {
           this.body.checkCollision.none = false;
           this.setVisible(true);
           setSound.playSE(1);
-          if (this.texture.key === "magic2") {
+          if (this.texture.key !== "magic2_2_1") {
+            if (this.fairy.evo2) {
+              this.anims.play("magic2_2");
+            } else if (this.fairy.evo1) {
+              this.anims.play("magic2_1");
+            } else {
+              this.anims.play("magic2");
+            }
+          }
+          if (this.texture.key === "magic2" || this.texture.key === "magic2_1" || this.texture.key === "magic2_2") {
             this.setVelocity(0, 0);
           }
         },
@@ -52,13 +61,50 @@ export default class Magic extends Phaser.Physics.Arcade.Sprite {
 
   bomb() {
     this.body.checkCollision.none = false;
-    this.setScale(this.fairy.spriteScale * 2);
 
+    this.setScale(this.fairy.spriteScale * 2);
+    let size = this.fairy.size * 2;
+    let hh = this.body.halfWidth;
+    let hw = this.body.halfHeight;
+    
+    if (this.fairy.level >= 3) {
+      size = this.fairy.size*1.3;
+    }
+
+    if (this.fairy.level >= 7) {
+      size = this.fairy.size*0.95;
+    }
+
+    this.setCircle(
+      hw * size,
+      hh - hw * size,
+      hh - hw * size
+    );
+    this.body.offset.x += 10;
+    this.body.offset.y += 11;
+
+    if (this.fairy.level >= 3) {
+
+      this.body.offset.x -= 10;
+      this.body.offset.y -= 11;
+    }
+    if (this.fairy.level >= 7) {
+
+      this.body.offset.x -= 14;
+      this.body.offset.y -= 15;
+    }
     if (!this.isBomb) {
       this.isBomb = true;
       this.fairy.bombCount++;
     }
-    this.anims.play("magic5_1", true);
+
+    if (this.fairy.level === 9) {
+      this.anims.play("magic5_3");
+    } else if (this.fairy.level >= 5) {
+      this.anims.play("magic5_2");
+    } else {
+      this.anims.play("magic5_1");
+    }
     thisScene.time.addEvent({
       delay: this.fairy.range * 1000,
       callback: () => {
