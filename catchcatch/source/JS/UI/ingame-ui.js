8a1aac1717595e89afc903dd6181e90c95374829
최영선 <vnddns26@naver.com>
2022-11-08 10:25:02 +0900

    #1 :sparkles: 튜토리얼 중간 완료 및 쿨타임 UI 수정

--- catchcatch/source/JS/UI/ingame-ui.js
+++ catchcatch/source/JS/UI/ingame-ui.js
@@ -5,8 +5,6 @@ let _timerTxt = document.createElement("div");
 let _minute = 0;
 let _second = 0;
 
-let _catCoin;
-
 export default function ingameUi() {
   const fairy = [wizard, reaper, ninja, slime, witch];
   const fairyName = ["wizard", "reaper", "ninja", "slime", "witch"];
@@ -27,79 +25,12 @@ export default function ingameUi() {
   const gameContainer = document.querySelector("#game-container");
   // const progress = document.createElement("progress");
 
-  const holeHp = document.createElement("div");
-  holeHp.setAttribute("class", "holeHp");
-  const hp = document.createElement("div");
-  hp.setAttribute("class", "hp");
-  hp.innerText = hole.hp;
-  holeHp.appendChild(hp);
-  gameContainer.appendChild(holeHp);
-
-  _catCoin = document.createElement("div");
-  _catCoin.setAttribute("class", "catCoin");
-  _catCoin.setAttribute("id", "catCoin");
-  _catCoin.textContent = player.coin + " Coin";
-
-  const stats = document.createElement("div");
-  const heal = document.createElement("div");
-  const healTxt = document.createElement("div");
-
-  const dmgMul = document.createElement("div");
-  const dmgMulTxt = document.createElement("div");
-
-  const speed = document.createElement("div");
-  const speedTxt = document.createElement("div");
-
   _timerTxt.setAttribute("class", "Timer");
   gameContainer.appendChild(_timerTxt);
   _minute = 0;
   _second = 0;
 
   _timerTxt.textContent = `${_minute}:${_second}`;
-
-  // progress.setAttribute("id", "progress");
-  // progress.setAttribute("value", player.exp);
-  // progress.setAttribute("max", 100);
-  stats.setAttribute("class", "stats");
-  heal.setAttribute("class", "stat");
-  healTxt.setAttribute("class", "healTxt");
-
-  dmgMul.setAttribute("class", "stat");
-  dmgMulTxt.setAttribute("class", "dmgMulTxt");
-
-  speed.setAttribute("class", "stat");
-  speedTxt.setAttribute("class", "speedTxt");
-
-  heal.setAttribute("id", "heal");
-  heal.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  heal.style.backgroundPosition = "center";
-  heal.style.backgroundRepeat = "no-repeat";
-  heal.style.backgroundSize = "contain";
-
-  dmgMul.setAttribute("id", "dmgMul");
-  dmgMul.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  dmgMul.style.backgroundPosition = "center";
-  dmgMul.style.backgroundRepeat = "no-repeat";
-  dmgMul.style.backgroundSize = "contain";
-
-  speed.setAttribute("id", "speed");
-  speed.style.backgroundImage = "url('images/ui/Icon/sample.png')";
-  speed.style.backgroundPosition = "center";
-  speed.style.backgroundRepeat = "no-repeat";
-  speed.style.backgroundSize = "contain";
-
-  healTxt.innerText = `Lv.${player.healLevel}`;
-  heal.appendChild(healTxt);
-  dmgMulTxt.innerText = `Lv.${player.dmgMulLevel}`;
-  dmgMul.appendChild(dmgMulTxt);
-  speedTxt.innerText = `Lv.${player.speedLevel}`;
-  speed.appendChild(speedTxt);
-
-  stats.appendChild(_catCoin);
-  stats.appendChild(heal);
-  stats.appendChild(dmgMul);
-  stats.appendChild(speed);
-  // gameContainer.appendChild(progress);
 
   // 쿨타임
   const coolContainer = document.createElement("div");
@@ -152,13 +83,6 @@ export function updateExp() {
   dmgMul.innerText = `Lv.${player.dmgMulLevel}`;
   speed.innerText = `Lv.${player.speedLevel} `;
   // progress.setAttribute("value", player.exp);
-}
-
-export function updateHP() {
-  // console.log(hole.hp);
-  const holeHp = document.querySelector(".holeHp");
-  const hp = document.querySelector(".hp");
-  hp.innerText = hole.hp;
 }
 
 export function GameOver() {
@@ -235,10 +159,6 @@ function GameClear() {
 
 function GoHome() {
   window.location.reload();
-}
-
-export function UpdateCatCoin() {
-  _catCoin.textContent = player.coin + " Coin";
 }
 
 export function useSkill(num) {
