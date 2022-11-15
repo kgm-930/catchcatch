db4197829a34c9ca5726e13afef93c30607cbee2
최영선 <vnddns26@naver.com>
2022-11-15 09:20:38 +0900

    #1 :sparkles: pet levelup 시 levelCount 증가

--- catchcatch/source/JS/UI/levelup.js
+++ catchcatch/source/JS/UI/levelup.js
@@ -183,6 +183,7 @@ export default function levelup() {
       }
     }
   }
+  console.log(levelCount, randomIndexArray);
   const gameContainer = document.querySelector("#game-container");
 
   // const levelupContainer = document.createElement("div");
