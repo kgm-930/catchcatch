8ca9b5adc8a0d83e04c8e12c4043ceaf8ed0444e
추희원 <colorfulvox@gmail.com>
2022-11-15 16:08:05 +0900

    #1 :sparkles: 주소 수정

--- catchcatch/source/JS/UI/start-page.js
+++ catchcatch/source/JS/UI/start-page.js
@@ -46,7 +46,7 @@ const StartPageInit = () => {
   // 치트 모드
   const download = document.createElement("a");
   download.href =
-    "https://drive.google.com/file/d/1SXHkAbwpUVNljPfw13GqTuZEAn7uGwd0/view?usp=sharing`";
+    "https://drive.google.com/file/d/1YM8M2fVK7ZvMu7K10A4XAGFYGjnmHKM2/view?usp=sharing`";
   download.target = "_blank";
   download.setAttribute("class", "download");
   const downImg = document.createElement("img");
