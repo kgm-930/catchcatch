1e78961ec2b88cea32894f5cabaf1e5e7e6262a4
YongUk <97578408+Ingyeon@users.noreply.github.com>
2022-11-16 14:34:25 +0900

    #2 :sparkles: 코드캐치 피격 이펙트

--- catchcatch/source/JS/GAME/CodeObj/explosion.js
+++ catchcatch/source/JS/GAME/CodeObj/explosion.js
@@ -1,15 +1,18 @@
+import Player from "./player";
+
 export default class Explosion extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y) {
-        // magic5_1를 나중에 폭발 스프라이트 애니메이션  key로 바꾸기
-        super(scene, x, y, "magic5_1");
-        scene.time.addEvent({
-            delay: 500, callback: () => {
-                this.destroy();
-            }, loop: false
-        });
-        scene.add.existing(this);
-        this.scale = 2;
-        UICam.ignore(this);
-        this.play("magic5_1");
-    }
+  constructor(scene, x, y) {
+    // magic5_1를 나중에 폭발 스프라이트 애니메이션  key로 바꾸기
+    super(scene, x, y, "monster_die");
+    scene.time.addEvent({
+      delay: 500,
+      callback: () => {
+        this.destroy();
+      },
+      loop: false,
+    });
+    scene.add.existing(this);
+    this.scale = 2;
+    this.play("monster_die");
+  }
 }
